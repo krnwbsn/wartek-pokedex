7fb43ddf3fb6f8e113b777cedbc454b3191975f1
krina <krina.wibisana@tanihub.com>
2021-11-15 01:13:41 +0700

    [UPDATE] Router to useRouter()

--- components/Layout/Component.tsx
+++ components/Layout/Component.tsx
@@ -2,33 +2,37 @@ import React from 'react';
 import Images from 'next/image';
 import * as Styled from './styles';
 import Loader from 'react-loader-spinner';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
 
 export interface ILayout {
   children: JSX.Element;
   loading: boolean;
 }
 
-const Component = ({ children, loading }: ILayout) => (
-  <Styled.Container>
-    <Styled.Header>
-      <Images
-        src="/pokemon-logo.png"
-        alt="Pokemon Logo"
-        width="154px"
-        height="57px"
-        onClick={() => Router.push('/')}
-      />
-    </Styled.Header>
-    <Styled.Content>
-      {loading && (
-        <Styled.LoadingContainer>
-          <Loader type="ThreeDots" color="#ffcb05" height={200} width={200} />
-        </Styled.LoadingContainer>
-      )}
-      <div>{children}</div>
-    </Styled.Content>
-  </Styled.Container>
-);
+const Component = ({ children, loading }: ILayout) => {
+  const Router = useRouter();
+
+  return (
+    <Styled.Container>
+      <Styled.Header>
+        <Images
+          src="/pokemon-logo.png"
+          alt="Pokemon Logo"
+          width="154px"
+          height="57px"
+          onClick={() => Router.push('/')}
+        />
+      </Styled.Header>
+      <Styled.Content>
+        {loading && (
+          <Styled.LoadingContainer>
+            <Loader type="ThreeDots" color="#ffcb05" height={200} width={200} />
+          </Styled.LoadingContainer>
+        )}
+        <div>{children}</div>
+      </Styled.Content>
+    </Styled.Container>
+  );
+};
 
 export default Component;
